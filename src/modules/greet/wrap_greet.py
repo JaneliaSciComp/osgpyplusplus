074ef89bc520359c4ddd04501b5da82af68dd07c
Christopher Bruns <brunsc@janelia.hhmi.org>
2014-08-14 15:14:49 -0400

    Rearrange module folder structure

---
+++ src/modules/greet/wrap_greet.py
@@ -0,0 +1,8 @@
+from pyplusplus import module_builder
+import os
+
+mb = module_builder.module_builder_t(
+    files = ["greet.h",],
+    gccxml_path = "C:/Program Files (x86)/gccxml/bin/gccxml.exe")
+mb.build_code_creator(module_name='greet')
+mb.split_module(os.path.join(os.path.abspath('.'), 'generated_code'))
